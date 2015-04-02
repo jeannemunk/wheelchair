0c83c892466900b4af87370ab844ab0ab605ba94
root <root@beaglebone.(none)>
2015-04-02 23:08:30 +0000

    Added header file for controls

---
+++ Test/ControlTest/control.h
@@ -0,0 +1,33 @@
+// Control Header File
+// Authored by: Clint Fernelius
+//              Jeanne Munk
+//              Tyler Travis
+//
+
+#ifndef CONTROL_H
+#define CONTROL_H
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Control
+{
+    private:
+        float fbV;
+        float lrV;
+
+        int pixelD;     // FB Control
+        int radiusD;    // LR Control
+        
+    public:
+        float getFBvoltage();
+        float getLRvoltage();
+
+        void setFBvoltage(float);
+        void setLRvoltage(float);
+
+        void fbControl(int);
+        void lrControl(int); 
+};
